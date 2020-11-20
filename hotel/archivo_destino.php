3bc2e56aaacbf76b82c63126799065f18feb63e5
soltrep <soltrep90@gmail.com>
2020-11-20 20:55:06 -0300

    usuarios pueden loguearse

--- hotel/archivo_destino.php
+++ hotel/archivo_destino.php
@@ -1,40 +1,26 @@
 <?php
-//se lee la cookie de sesion
-session_start();
-//se establece una conexión a la base de datos
-include 'conexion.php';
-//se validarán los campos si la sesion aún no está abierta
+if(isset($_POST['datos_introducidos_email']) && isset($_POST['datos_introducidos_password'])) {
+    //se establece una conexión a la base de datos
+    include 'conexion.php';
+    //se validarán los campos si la sesion aún no está abierta
 
-
-if (empty($_SESSION) and isset($_POST['datos_introducidos_email']) and isset($_POST['datos_introducidos_password'])){
     //se escaparán caracteres peligrosos
     $email=mysqli_real_escape_string($conexion,$_POST['datos_introducidos_email']);
-    $contraseña_introducida=$_POST['datos_introducidos_password'];
+    $contraseña_introducida=htmlentities(htmlspecialchars($_POST['datos_introducidos_password']));
+    $password= md5(crc32(sha1($contraseña_introducida)));
     //se hará la consulta a la base de datos
-    $consulta='select * from usuarios where email="'.$email.'"';
+    $consulta='SELECT * FROM usuarios WHERE email="'.$email.'" AND pass="'.$password.'";';
     //si hubo un resultado
-    if ($ejecución_de_la_consulta=$conexion->query($consulta)){
-        //se obtiene la contraseña registrada
-        
-        $contraseña_guardada=$ejecución_de_la_consulta->fetch_assoc();
-        //var_dump($contraseña_guardada['nombre']) or die();
-        //se compara la contraseña
-        $verificar_contraseña=password_verify($contraseña_introducida,$contraseña_guardada['password']);
-        //si el resultado de la comparación ha sido verdadero
-        if ($verificar_contraseña){
-            //se asigna la sesión y redirecciona
-            $_SESSION['name']=$contraseña_guardada['nombre'];
-            header ('location: home.php');
-        }//si la contraseña es incorrecta
-        else{
-            header ('location: ./');
-        }
-    }//si el usuario no está registrado
-    else{
-        header ('location: ./');
+    if ($r = $conexion->query($consulta)) {
+        $r = mysqli_fetch_array($r, MYSQLI_ASSOC);
+        session_start();
+        $_SESSION['name']=$r['nombre'];
+        header ('location: home.php');
+    } else {
+        header("Location: principal.php");
     }
-}//si hay una sesion abierta o no se enviaron datos
-else{
-    header ('location: cerrar_sesion.php');
+} else {
+    header("Location: principal.php");
 }
+
 ?>
