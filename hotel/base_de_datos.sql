4596812e4f77affa25c88ef1b25afffb30652c0c
soltrep <soltrep90@gmail.com>
2020-11-20 18:47:31 -0300

    se agrego un nuevo campo a la tabla de usuarios

--- hotel/base_de_datos.sql
+++ hotel/base_de_datos.sql
@@ -1,10 +1,13 @@
 -- creación de la base de datos
+drop database if exists ohana;
 create database ohana;
 use ohana;
 -- creación de la tabla de registros
+drop table if exists usuarios;
 create table usuarios(
     -- creación de las columnas
     id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR (255) NOT NULL,
-    password VARCHAR (255) NOT NULL
+    nombre varchar(255) not null,
+    email varchar(255) not null,
+    pass varchar (255) not null
 )Engine InnoDB default charset=latin1;
